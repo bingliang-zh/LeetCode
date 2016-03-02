59926f661fcb01788e8d2de4ce1a4ef31f493712
秉亮 <bl_indie@qq.com>
2016-03-02 14:03:50 +0800

    292

---
+++ 292.Nim_Game.rb
@@ -0,0 +1,9 @@
+# encoding: UTF-8
+# author: bl_indie
+# link: https://leetcode.com/problems/nim-game/
+# Your runtime beats 35.85% of rubysubmissions.
+# @param {Integer} n
+# @return {Boolean}
+def can_win_nim(n)
+  n%4 != 0
+end
